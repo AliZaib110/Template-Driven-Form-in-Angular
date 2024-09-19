9bd9d1cf3ed8762970f764ba72fa01eade4706a7
alizaib110 <alizaibbuttar9@gmail.com>
2024-09-19 22:32:52 +0500

    template driven form created and bootstrap install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { log } from 'console';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'templateDrivenForm';
+  name: any;
+  email: any;
+  password: any;
+
+  onSubmit(data: NgForm) {
+    if (data.valid) {
+      console.log(data.value);
+    }
+  }
 }
